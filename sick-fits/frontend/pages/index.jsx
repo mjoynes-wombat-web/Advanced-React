206bfcb93b18b141f04bbd8082f47f4dc64387d4
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-22 10:51:48 -0700

    Setup home page.

---
+++ sick-fits/frontend/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Home = props => (
+  <>
+    <p>Hey</p>
+    <a href="/sell">Sell</a>
+  </>
+);
+
+Home.propTypes = {
+
+};
+
+export default Home;
