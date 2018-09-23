80b11c1eca5ca756315db8b6a72b3baf45244a61
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-23 13:38:00 -0700

    Added nav links and nav styling.

--- sick-fits/frontend/components/Nav.jsx
+++ sick-fits/frontend/components/Nav.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
+import NavStyles from './styles/NavStyles';
+
 const Nav = props => (
-  <div>
+  <NavStyles>
     <Link href="/">
-      <a>Home</a>
+      <a>Sell</a>
+    </Link>
+    <Link href="/items">
+      <a>Items</a>
     </Link>
     <Link href="/sell">
       <a>Sell</a>
     </Link>
-  </div>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 );
 
 Nav.propTypes = {
