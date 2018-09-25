abc584f22588898c8df56f24070d501cc8c3a9b5
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-24 20:48:38 -0700

    Modified navigation.

--- sick-fits/frontend/components/Nav.jsx
+++ sick-fits/frontend/components/Nav.jsx
@@ -7,7 +7,7 @@ import NavStyles from './styles/NavStyles';
 const Nav = props => (
   <NavStyles>
     <Link href="/items">
-      <a>Items</a>
+      <a>Shop</a>
     </Link>
     <Link href="/sell">
       <a>Sell</a>
