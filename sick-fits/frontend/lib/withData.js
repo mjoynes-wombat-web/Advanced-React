8dccbc5c90d5321d5d11286aa4c6a4e4f8492a79
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-24 20:47:50 -0700

    Hooked up data to the frontend.

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,11 +1,12 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
+
 import { endpoint } from '../config';
 
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
+    request: (operation) => {
       operation.setContext({
         fetchOptions: {
           credentials: 'include',
--- sick-fits/frontend/pages/_app.jsx
+++ sick-fits/frontend/pages/_app.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import PropTypes from 'prop-types';
+import { ApolloProvider } from 'react-apollo';
 
+import withData from '../lib/withData';
 import Page from '../components/Page';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // This exposes the query to user.
+    pageProps.query = ctx.query;
+
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
@@ -21,4 +36,4 @@ MyApp.propTypes = {
 
 };
 
-export default MyApp;
+export default withData(MyApp);
