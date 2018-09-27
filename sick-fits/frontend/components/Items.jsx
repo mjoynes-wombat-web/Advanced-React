6d1b6d702b468fb715863da053681ab099a05f13
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-26 19:38:56 -0700

    Added query params for pagination.

--- sick-fits/frontend/components/Items.jsx
+++ sick-fits/frontend/components/Items.jsx
@@ -6,10 +6,11 @@ import styled from 'styled-components';
 
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -37,7 +38,12 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: (page * perPage) - perPage,
+          }}
+        >
           {({
             data: { items },
             error,
