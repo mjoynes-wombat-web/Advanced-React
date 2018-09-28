b362c1b4327437d40bcf416235a2118f11bc8b36
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-27 17:06:52 -0700

    Added signup query.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -25,6 +28,27 @@ const Mutations = {
     const item = await ctx.db.query.item({ where }, '{ title id description }');
 
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    const email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        email,
+        password,
+        permissions: { set: ['USER'] },
+      },
+    }, info);
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+
+    return user;
   },
 };
 
